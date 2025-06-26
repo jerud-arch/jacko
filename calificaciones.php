3de7977eb8cdcac5012658ef6212b817564eb680
Hansel-Rhom <ozwinhim@gmail.com>
2025-06-26 13:07:22 -0600

    CALF
    JACK-O

---
+++ calificaciones.php
@@ -0,0 +1,96 @@
+<html>
+    <?php
+    include("head.html");
+    ?>
+
+    <body>
+     <?php
+    include("header.html");
+    ?>
+    
+        <?php
+    include("main.html");
+    ?>
+        <div class="container">
+ 
+            <div class="item-left">
+                <a href="menu.php"><img src="assets/svg/back.svg" alt="Regresar" height="40"></a>            
+            </div>  
+
+                        <h1>Horario Escolar</h1>
+            <table>
+                <tr>
+                    <th>Materia</th>
+                    <th>Unidad 1</th>
+                    <th>Unidad 2</th>
+                    <th>Unidad 3</th>
+                    <th>Unidad 4</th>
+                    <th>Total</th>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+
+                <tr>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                    <td>    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
